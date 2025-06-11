6a6ba9a4cab651c303a95be5978296f51e5b5849
henriqueeuler <henriqueeuler@hotmail.com>
2025-06-11 11:05:07 -0300

    Corrige sintaxe no Database.php

--- models/Database.php
+++ models/Database.php
@@ -1,30 +1,18 @@
 <?php
 class Database {
-<<<<<<< HEAD
-    private $host = 'db'; // nome do serviço definido no docker-compose.yml
-=======
-    private $host = 'db';
->>>>>>> 819191369813a03488be523bbb05e6a6f987d4ff
+    private $host = 'localhost';
     private $dbname = 'securetickets';
-    private $user = 'postgres';
-    private $password = 'postgres';
-    public $pdo;
+    private $user = 'root';
+    private $pass = '';
 
-    public function __construct() {
+    public function connect() {
         try {
-<<<<<<< HEAD
-            $this->pdo = new PDO(
-                "pgsql:host=$this->host;dbname=$this->dbname",
-                $this->user,
-                $this->password
-            );
-=======
-            $this->pdo = new PDO("pgsql:host=$this->host;dbname=$this->dbname", $this->user, $this->password);
->>>>>>> 819191369813a03488be523bbb05e6a6f987d4ff
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
         } catch (PDOException $e) {
-            die("Erro de conexão: " . $e->getMessage());
+            die("Erro na conexão: " . $e->getMessage());
         }
     }
 }
-?>
+
