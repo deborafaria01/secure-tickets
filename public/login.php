9fa80ab15db2f54e71aa61baf836b920f2d2bba7
henriqueeuler <henriqueeuler@hotmail.com>
2025-06-11 10:44:19 -0300

    Testando deploy automático

--- public/login.php
+++ public/login.php
@@ -12,7 +12,7 @@ $error = $_GET['error'] ?? '';
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
-  <title>Login - SecureTickets</title>
+  <title>Login-SecureTickets</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="d-flex align-items-center justify-content-center vh-100 bg-light">
