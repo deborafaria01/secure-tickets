36cc24a67c7f13418181ca90ca35284c40fcd12b
henriqueeuler <henriqueeuler@hotmail.com>
2025-06-11 11:24:47 -0300

    Testando deploy automático

--- public/logout.php
+++ public/logout.php
@@ -1,13 +1,5 @@
 <?php
 session_start();
-<<<<<<< HEAD
-session_unset();
 session_destroy();
 header("Location: login.php");
 exit();
-=======
-session_destroy();
-header('Location: login.php');
-exit();
-?>
->>>>>>> 819191369813a03488be523bbb05e6a6f987d4ff
