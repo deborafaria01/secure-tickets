9610f4a11226e79d8ce7470871951b06d89d2278
henriqueeuler <henriqueeuler@hotmail.com>
2025-06-11 11:23:19 -0300

    Testando deploy automático

--- public/relatorios.php
+++ public/relatorios.php
@@ -9,9 +9,10 @@ require_once '../models/Database.php';
 include 'includes/navbar.php';
 
 $db = new Database();
+$pdo = $db->connect(); // Correção aqui
 
 try {
-    $stmt = $db->pdo->query("SELECT nome, data_evento FROM eventos ORDER BY data_evento ASC");
+    $stmt = $pdo->query("SELECT nome, data_evento FROM eventos ORDER BY data_evento ASC");
     $eventos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     die("Erro ao buscar eventos: " . $e->getMessage());
@@ -51,4 +52,5 @@ try {
 
 </body>
 </html>
+
 
